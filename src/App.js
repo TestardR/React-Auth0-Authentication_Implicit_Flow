f15e2c9003e9befebd98f0772d551bde02ac3aab
rtestard <rtestard@opensense.fr>
2019-07-24 15:49:37 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Route, Redirect } from "react-router-dom";
+import "./App.css";
+import Home from "./Home";
+import Profile from "./Profile";
+import Nav from "./Nav";
+import Auth from "./Auth";
+import Callback from "./Callback";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.auth = new Auth(this.props.history);
+  }
+
+  render() {
+    return (
+      <>
+        <Nav auth={this.auth} />
+        <Route
+          path="/"
+          exact
+          render={props => <Home auth={this.auth} {...props} />}
+        />
+        <Route
+          path="/callback"
+          render={props => <Callback auth={this.auth} {...props} />}
+        />
+        <Route
+          path="/profile"
+          render={props =>
+            this.auth.isAuthenticated() ? (
+              <Profile auth={this.auth} {...props} />
+            ) : (
+              <Redirect to="/" />
+            )
+          }
+        />
+      </>
+    );
+  }
 }
 
 export default App;
